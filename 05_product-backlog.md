a58a6837a8787b43ac2fef6251e0659d64b57fd2
thanyaboonaumpong <thanyaboonaumpong@hotmail.com>
2026-03-10 13:07:00 +0700

    V.1 my-ecommerce

---
+++ 05_product-backlog.md
@@ -0,0 +1,2 @@
+#markdown
+#เขียนเป็นBullet-point
---
+++ 06_bring-it-to-javascript.js
@@ -0,0 +1,33 @@
+##Show your business entities in JavaScript file.
+##Example:
+##string for product name
+##number for price
+##boolean for inStock
+##array for list of products
+##object for one product or 
+##object for one order
+##array of objects for shopping cart 
+## -------------------------------------------------------------------------
+let productList = ["product_id_1, "product_id_2", "product_id_3"];
+
+    product_A = {
+        productId: "product_id_1",
+        productName: "Macbook",
+        price: 199,
+    };
+
+    let cart = [
+  {
+    productId: "product_id_1",
+    quantity: 10,
+  },
+  {
+    productId: "product_id_2",
+    quantity: 5,
+  },
+];
+
+
+    console.log(productList);
+    console.log(product_A);
+    console.log(cart);
